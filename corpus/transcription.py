d636ea4f206b0f0bb97b8fac2d6d4711903ce362
Angelica Huyen Tran <tranac@usc.edu>
2016-11-30 23:15:22 -0800

    Script to create transcription of files.

---
+++ corpus/transcription.py
@@ -0,0 +1,42 @@
+import csv
+
+num_max = 1
+num_mat = 1
+num_ang = 1
+
+with open("transcription_output.txt", "w") as output:
+	with open("input.csv","r") as input:
+		reader = csv.DictReader(input)
+		for entry in reader:
+			# File Tag
+			if entry["Person"] == "MW":
+				output.write("MAX_")
+				if(num_max < 10):
+					output.write("0")
+				if(num_max < 100):
+					output.write("0")
+				output.write(str(num_max) + " ")
+				num_max += 1
+			elif entry["Person"] == "MS":
+				output.write("MAT_")
+				if(num_mat < 10):
+					output.write("0")
+				if(num_mat < 100):
+					output.write("0")
+				output.write(str(num_mat) + " ")
+				num_mat += 1
+			else:
+				output.write("ANG_")
+				if(num_ang < 10):
+					output.write("0")
+				if(num_ang < 100):
+					output.write("0")
+				num_ang += 1
+				output.write(str(num_ang) + " ")
+			# Sentence
+			sentence = entry["Sentence"]
+			sentence = sentence.upper()[:-1]
+			print(sentence)
+			output.write(sentence + '\n')
+	input.closed
+output.closed
